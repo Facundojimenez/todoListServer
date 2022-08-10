37b3589f782ede44e825a14359e5bc2343ca85ae
unknown <facundo.gabriel.jimenez@gmail.com>
2022-08-10 11:33:04 -0300

    fix mongo connect

--- src/db/mongoConnection.js
+++ src/db/mongoConnection.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-mongoose.connect(process.env.MONGO_CONNECTION_STRING);
+mongoose.connect("mongodb+srv://facundoj:Facu2000@cluster0.1w4mu.mongodb.net/DB-todolist?authSource=admin&replicaSet=atlas-rbr49r-shard-0&readPreference=primary&ssl=true");
 
 mongoose.connection.on("open", () => {
     console.log("Base de MONGO conectada");
